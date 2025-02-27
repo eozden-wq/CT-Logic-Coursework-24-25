3874b1b36328d95b7b42407980002f52b9269e01
Emre Ozden <tlcj35@durham.ac.uk>
2025-02-27 09:18:27 +0000

    Added coursework code template to the repo

--- code/main.py
+++ code/main.py
@@ -1,6 +1,92 @@
-def main():
-    print("Hello, World!")
+
+def load_dimacs(file_name):
+    #file_name will be of the form "problem_name.txt"
+    ...
 
 
-if __name__ == "__main__":
-    main()
+def simple_sat_solve(clause_set):
+    ...
+
+
+def branching_sat_solve(clause_set,partial_assignment):
+    ...
+
+
+def unit_propagate(clause_set):
+    ...
+
+
+def dpll_sat_solve(clause_set,partial_assignment):
+    ...
+
+
+
+def test():
+    print("Testing load_dimacs")
+    try:
+        dimacs = load_dimacs("sat.txt")
+        assert dimacs == [[1],[1,-1],[-1,-2]]
+        print("Test passed")
+    except:
+        print("Failed to correctly load DIMACS file")
+
+    print("Testing simple_sat_solve")
+    try:
+        sat1 = [[1],[1,-1],[-1,-2]]
+        check = simple_sat_solve(sat1)
+        assert check == [1,-2] or check == [-2,1]
+        print("Test (SAT) passed")
+    except:
+        print("simple_sat_solve did not work correctly a sat instance")
+
+    try:
+        unsat1 = [[1, -2], [-1, 2], [-1, -2], [1, 2]]
+        check = simple_sat_solve(unsat1)
+        assert (not check)
+        print("Test (UNSAT) passed")
+    except:
+        print("simple_sat_solve did not work correctly an unsat instance")
+
+    print("Testing branching_sat_solve")
+    try:
+        sat1 = [[1],[1,-1],[-1,-2]]
+        check = branching_sat_solve(sat1,[])
+        assert check == [1,-2] or check == [-2,1]
+        print("Test (SAT) passed")
+    except:
+        print("branching_sat_solve did not work correctly a sat instance")
+
+    try:
+        unsat1 = [[1, -2], [-1, 2], [-1, -2], [1, 2]]
+        check = branching_sat_solve(unsat1,[])
+        assert (not check)
+        print("Test (UNSAT) passed")
+    except:
+        print("branching_sat_solve did not work correctly an unsat instance")
+
+
+    print("Testing unit_propagate")
+    try:
+        clause_set = [[1],[-1,2]]
+        check = unit_propagate(clause_set)
+        assert check == []
+        print("Test passed")
+    except:
+        print("unit_propagate did not work correctly")
+
+
+    print("Testing DPLL") #Note, this requires load_dimacs to work correctly
+    problem_names = ["sat.txt","unsat.txt"]
+    for problem in problem_names:
+        try:
+            clause_set = load_dimacs(problem)
+            check = dpll_sat_solve(clause_set,[])
+            if problem == problem_names[1]:
+                assert (not check)
+                print("Test (UNSAT) passed")
+            else:
+                assert check == [1,-2] or check == [-2,1]
+                print("Test (SAT) passed")
+        except:
+            print("Failed problem " + str(problem))
+    print("Finished tests")
