728ade7d54f40190169172a8719726e2df737599
Emre Ozden <tlcj35@durham.ac.uk>
2025-02-27 20:34:51 +0000

    Wrote the simple_sat_solve function and checked whether each line in DIMACS ends in 0 (don't know if I need to do DIMACS validation)

--- code/main.py
+++ code/main.py
@@ -6,15 +6,50 @@ def load_dimacs(file_name):
         raw_clause_set = [line.split() for line in dimacs_file]
         raw_clause_set.pop(0)
         for clause in raw_clause_set:
-            clause.pop()
+            if clause[-1] == '0':
+                clause.pop()
+            else:
+                raise Exception("Invalid DIMACS format")
             clause_set.append(list(map(int, clause)))
         
     return clause_set
 
 
 def simple_sat_solve(clause_set):
-    ...
+    num_vars = 0
+    for clause in clause_set:
+        max_num_in_clause = abs(max(clause, key=abs))
+        if max_num_in_clause > num_vars:
+            num_vars = max_num_in_clause
+    
+    res = []
+    for i in range(2**num_vars):
+        assignment = list(map(int, format(i, f'0{num_vars}b')))
+        satisfied = True
+        for clause in clause_set:
+            clause_result = 0
+            # compute disjunction of literals in given clause
+            for var in clause:
+                if var < 0:
+                    clause_result |= not(assignment[abs(var) - 1]) 
+                else:
+                    clause_result |= assignment[abs(var) - 1]
+            
+            if clause_result == 0:
+                satisfied = False
+                break
+            else:
+                continue
+        
+        if satisfied:
+            for i,val in enumerate(assignment):
+                if not val:
+                    res.append(-1 * (i + 1))
+                else:
+                    res.append(i + 1)
+            return res
 
+    return False
 
 def branching_sat_solve(clause_set,partial_assignment):
     ...
